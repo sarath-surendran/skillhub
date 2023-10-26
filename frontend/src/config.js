11212eb2f8bb0cc431dd352c0134cec7381d0a5e
Sarath <sarathcm)@gmail.com>
2023-10-26 13:56:05 +0530

    twelvth step for nginx

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -8,9 +8,9 @@
 const config = {
 //   apiUrl: "http://127.0.0.1:8000/",
 //   baseUrl: "http://127.0.0.1:8000/api",
-    media_url: "http://13.53.122.250",
-    socket_url: "ws://13.53.122.250",
-    axios_url: "http://13.53.122.250/api/",
+    media_url: "https://13.53.122.250",
+    socket_url: "wss://13.53.122.250",
+    axios_url: "https://13.53.122.250/api/",
 };
 
 
