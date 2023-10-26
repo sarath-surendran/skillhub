095555dc2e62f01f919003c609f7fa7d187775ed
Sarath <sarathcm)@gmail.com>
2023-10-26 09:26:43 +0530

    first step for nginx

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -11,14 +11,14 @@ urlpatterns = [
     path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/',TokenVerifyView.as_view(),name='token_verify'),
-    path('users/', include('users.urls')),
-    path('courses/', include('courses.urls')),
-    path('enrollments/',include('enrollments.urls')),
-    path('profile/', include('userprofile.urls')),
-    path('review&ratings/',include('review_ratings.urls')),
-    path('admin_user/', include("admin_user.urls")),
-    path('course_progress/', include('course_progress.urls')),
-    path('chats/',include("chats.urls")),
+    path('api/users/', include('users.urls')),
+    path('api/courses/', include('courses.urls')),
+    path('api/enrollments/',include('enrollments.urls')),
+    path('api/profile/', include('userprofile.urls')),
+    path('api/review&ratings/',include('review_ratings.urls')),
+    path('api/admin_user/', include("admin_user.urls")),
+    path('api/course_progress/', include('course_progress.urls')),
+    path('api/chats/',include("chats.urls")),
 ]
 
 if settings.DEBUG:
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,9 +1,16 @@
+// const config = {
+// //   apiUrl: "http://127.0.0.1:8000/",
+// //   baseUrl: "http://127.0.0.1:8000/api",
+//   media_url: "http://127.0.0.1:8000",
+//   socket_url: "ws://127.0.0.1:8000",
+//   axios_url: "http://127.0.0.1:8000/api/",
+// };
 const config = {
 //   apiUrl: "http://127.0.0.1:8000/",
 //   baseUrl: "http://127.0.0.1:8000/api",
-  media_url: "http://127.0.0.1:8000",
-  socket_url: "ws://127.0.0.1:8000",
-  axios_url: "http://127.0.0.1:8000/",
+    media_url: "http://13.53.122.250",
+    socket_url: "ws://13.53.122.250",
+    axios_url: "http://13.53.122.250/api/",
 };
 
 
