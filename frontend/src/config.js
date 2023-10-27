7e4e51e1aaf12491574a8e2a858569e88d279089
Sarath <sarathcm)@gmail.com>
2023-10-27 09:28:34 +0530

    38 step for nginx

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -8,9 +8,9 @@
 const config = {
 //   apiUrl: "http://127.0.0.1:8000/",
 //   baseUrl: "http://127.0.0.1:8000/api",
-    media_url: "http://16.171.160.157",
-    socket_url: "ws://16.171.160.157",
-    axios_url: "http://16.171.160.157/api/",
+    media_url: "https://16.171.160.157",
+    socket_url: "wss://16.171.160.157",
+    axios_url: "https://16.171.160.157/api/",
 };
 
 
